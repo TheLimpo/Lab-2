bfbd3ea62522a17bf0ea664221d4cce309df5c58
Linus Lundberg <linluw-3@student.ltu.se>
2019-11-18 19:38:36 +0100

    Task 1 done

--- Task1-SortCheck/src/SortCheck.cpp
+++ Task1-SortCheck/src/SortCheck.cpp
@@ -2,15 +2,29 @@
 
 using namespace std;
 
-bool is_sorted()
+bool is_sorted(int arr[])
 {
+	
+	for (int i = 0; i < 4; i++)
+	{
+		if (arr[i] > arr[i + 1])
+			return false;
+	}
 
+	return true;
 }
 
 int main()
 {
+	int arr[5];
 
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Enter a number you want in your 5 length array: " << endl;
+		cin >> arr[i];
+	}
 
-	
+	cout << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << " " << endl; //Just a visual check
 
+	cout << (is_sorted(arr) ? "True" : "False") << endl;
 }
